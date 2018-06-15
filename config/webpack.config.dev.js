e93f1c5245f02bcc51612ed843fe79b288f5572d
Alex <>
2018-06-15 19:52:43 +1000

    eslint config files

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -72,7 +72,7 @@ module.exports = {
     // We placed these paths second because we want `node_modules` to "win"
     // if there are any conflicts. This matches Node resolution mechanism.
     // https://github.com/facebookincubator/create-react-app/issues/253
-    modules: ["node_modules", path.resolve(), paths.appNodeModules].concat(
+    modules: ["node_modules", paths.appNodeModules].concat(
       // It is guaranteed to exist because we tweak it in `env.js`
       process.env.NODE_PATH.split(path.delimiter).filter(Boolean)
     ),
@@ -86,7 +86,9 @@ module.exports = {
     alias: {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
-      "react-native": "react-native-web"
+      "react-native": "react-native-web",
+      "~": path.resolve(__dirname, "../src"),
+      src: path.resolve(__dirname, "../src")
     },
     plugins: [
       // Prevents users from importing files from outside of src/ (or node_modules/).
